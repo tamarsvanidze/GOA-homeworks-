bbe95a8e92cde57542012f26f75479fbd3981721
tamarsvanidze <tamarsanidze72@gmail.com>
2024-03-09 23:35:21 +0400

    sixth commit

---
+++ Day 5/homework1.py
@@ -0,0 +1,7 @@
+num1 = input("please enter first number: ")
+num2 = int(input("please enter second number: "))
+num3 = input("please enter third number:  ")
+
+print(type(num1))
+print(type(num2))
+print(type(num3))
