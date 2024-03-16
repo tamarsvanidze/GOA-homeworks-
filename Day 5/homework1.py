a2af94fdd5faaa80a6a705956603ee88ba1ccb8a
tamarsvanidze <tamarsanidze72@gmail.com>
2024-03-16 06:58:28 +0400

    seventh commit

--- Day 5/homework1.py
+++ Day 5/homework1.py
@@ -1,6 +1,11 @@
 num1 = input("please enter first number: ")
-num2 = int(input("please enter second number: "))
+
+num2 = int(input("please enter second number: ")) #გამოვიყენე int ფუნქცია, რომშემეცვალა მისთვის ტიპი
+#და არ ყოფილიყო ერთგვაროვანი
+
 num3 = input("please enter third number:  ")
+
+# ვნახოთ რა ტიპის ცვლადები შევქმენით
 
 print(type(num1))
 print(type(num2))
