8d0ccc4c73de03a15ef0a55814c0c130ed7beb33
tamarsvanidze <tamarsanidze72@gmail.com>
2024-03-28 18:40:57 +0400

    commit

--- Day 4/homrwork1.py
+++
@@ -1,5 +0,0 @@
-num1 = int(input("please enter first number:"))
-num2 = int(input("please enter second number:"))
-num3 = int(input("please enter third number:"))
-
-print(num1 + num2 + num3)
--- day 7/classwork.py
+++
@@ -1,7 +0,0 @@
-num1 = int(input(" enter your number: "))
-
-print(num1 >= 0 and num1 < 5)
- 
-num2 = int(input("enter your second number: "))
- 
-print(num2 > 10 and num2 % 2)
