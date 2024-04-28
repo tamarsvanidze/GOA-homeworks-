ded130786c7ec18d5b8ebfaa9ab09891d1e8340f
tamarsvanidze <tamarsanidze72@gmail.com>
2024-04-28 13:17:38 +0400

    commit

---
+++ Day 12/homework/homework.py
@@ -0,0 +1,25 @@
+#პირველი დავალება
+for i in range(10):
+    if i % 2 != 0:
+        print(i)
+
+# მეორე დავალება
+for i in range(51):
+    if i % 5 == 0:
+        print(i)
+
+#მესამე დავალება
+num = int(input("please enter whole number:"))
+for i in range(num):
+    print(i)
+
+ #მეოტხე დავალება
+result = 0 
+for i in range(50):
+    result += i
+
+print(result)
+
+
+
+
