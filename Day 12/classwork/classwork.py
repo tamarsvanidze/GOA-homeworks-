ebd222b5385756f1d0e10288d65e404819b899f5
tamarsvanidze <tamarsanidze72@gmail.com>
2024-05-05 23:23:03 +0400

    commit

---
+++ Day 12/classwork/classwork.py
@@ -0,0 +1,12 @@
+for i in range(10):
+    print("Tako")
+
+for i in range(10):
+    if i % 2 ==0:
+        print(i)
+
+result = 0
+for i in range(10):
+    if i % 2 ==0:
+        result += i
+print(result)  
