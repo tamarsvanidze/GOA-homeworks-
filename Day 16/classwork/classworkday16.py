79e8ca00addc76fc979b034761df415dcbe57856
tamarsvanidze <tamarsanidze72@gmail.com>
2024-05-25 20:42:43 +0400

    commit

---
+++ Day 16/classwork/classworkday16.py
@@ -0,0 +1,18 @@
+horror_movies = ["scream", "Talk to me", "Smile" , "Anabelle", "IT" ]
+fav_movie = horror_movies[4]
+print(fav_movie)
+
+cars = []
+
+#for i in range(5):
+ #   car = input("please enter your car brend: " )
+ #   cars.append(car)
+#cars[0] = "bugati"
+#deleted_item = cars.pop()
+#print(cars)
+
+cars = ["mersedes", "bmw", "ferarri", "porsche", "bugati"]
+count = 0
+for car in cars:
+    count = count+ 1
+print(count)    
