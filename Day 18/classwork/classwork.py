4a81533fa2b2bcf8dee826413f2eec136c538b6a
tamarsvanidze <tamarsanidze72@gmail.com>
2024-06-08 18:43:23 +0400

    commit

---
+++ Day 18/classwork/classwork.py
@@ -0,0 +1,10 @@
+nums = [1, 2 ,3, 4, 5, 6, 7, 8, 9, 10]
+print(nums[-1])
+print(nums[-2])
+print(nums[-3])
+
+print(nums[0:3])
+
+print(nums[3:10:3])
+
+
---
+++ Day 18/homework/homework.py
@@ -0,0 +1,10 @@
+symbols = ["$", "%", "^", "V"]
+#პირველი გზა : ჩვეულებრივი ინდექსები
+print(symbols[1])
+print(symbols[2])
+#მეორე გზა : უარყოფითი ინდექსები
+print(symbols[-3])
+print(symbols[-2])
+
+
+
