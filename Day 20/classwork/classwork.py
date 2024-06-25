115db4b420ef7019d7ee3517f77d1eb5ed25dcbf
tamarsvanidze <tamarsanidze72@gmail.com>
2024-06-25 23:50:08 +0400

    commit

---
+++ Day 20/classwork/classwork.py
@@ -0,0 +1,26 @@
+# def print_numbers():
+#    for i in range(1, 20+ 1):
+#        print(i)  #შევქმენი ფუნქცია 
+
+# print_numbers() #დავბეჭდე რიცხვები 1დან 20ის ჩათვლით
+
+# def print_numbers():
+#     sum=0
+#     for i in range(1, 20 +1):
+#         sum += i
+       
+# print_numbers()
+
+num1=int(input("please enter first number:"))
+num2 = int(input("please enter second number:"))
+
+def print_numbers(start, end):
+    for i in range(start, end +1):
+        print(start + end)
+
+print_numbers(num1, num2)
+
+def print_numbers(start, end):
+    for i in range(start, end+ 1):
+        print()
+
---
+++ Day 20/homework/homework.py
@@ -0,0 +1,53 @@
+# პირველი დავალება
+for i in range(0, 21):
+    print(i)
+
+ # მეორე დავალება
+for i in range(1,11):
+    print(i)
+
+#მესამე დავალება
+for i in range(1, 100+ 1):
+    if i % 2 == 0:
+        print(i)
+        
+for i in range(1 , 100 +1):
+           if i % 2 != 0:
+                print(i)
+
+ # მეოტხე დავალება
+num = int(input("please enter your number :"))
+sum = 0
+for i in range(num):
+    sum += i
+print(sum)                    
+
+# მეხუტე დავალება
+for i in range(1, 50 +1 ):
+     if i % 5 == 0:
+          print(i)
+
+num1 = 0
+while num1  < 20:
+     print(num1)
+     num1 = num1 + 1
+
+results = 0
+i = 1
+while i < 10 :
+    results += i
+    i = i + 1
+
+print(results) 
+
+num2 = int(input("please enter number :"))
+while  num2 < 10:
+     num2 = num2 + 1
+     if num2 == 7:
+      print("you are corect")
+     else:
+          print("you are wrong")
+       
+     
+    
+          
