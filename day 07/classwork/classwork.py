58118e3f71866068766cf3064c8660c2dfccb08e
tamarsvanidze <tamarsanidze72@gmail.com>
2024-07-16 15:56:41 +0400

    commit

--- day 07/classwork/classwork.py
+++ day 07/classwork/classwork.py
@@ -5,3 +5,4 @@ print(age > 0 and age < 30)
 num1 = int(input("enter your number: "))
 num2 = num1 + 20
 print(num1 < num2)
+# i love goa academy
--- day 07/homework/homework1.py
+++ day 07/homework/homework1.py
@@ -22,7 +22,7 @@ num1 = int(input("please enter your number:"))
 
 print(num1>10 and num1 % 9 ==0 )
 
-#მეხუტე დავალება
+#მეხუთე დავალება
 
 print(15 == 14 and 18 > 15)
 print(65>=54 and 12 == 12)
@@ -31,3 +31,4 @@ print(9 > 18 or 83 < 9)
 print(18 >= 2 or 49 < 17)
 print(43 == 94 or 76 > 15)
 print(102>=564 or 89 > 12)
+# მე ძალიან მომწონს GOAს აკადემია
