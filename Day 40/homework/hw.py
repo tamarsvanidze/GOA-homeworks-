ef6619e43475eef37240dfd503ca5a4c6bd851c3
tamarsvanidze <tamarsanidze72@gmail.com>
2024-11-13 19:06:42 +0400

    commit

---
+++ Day 40/homework/hw.py
@@ -0,0 +1,17 @@
+#fisrt homework
+def greed():
+    name = input("please enter your name: ")
+    print("hello," + name + "! you are welcome.")
+
+greed()    
+
+#second
+def sandwich_make(bread, ingredients):
+    print(f"your sandwich with {bread} bread and the ingredients: {','.join(ingredients)}.")
+    print("enjoy your meal <33")
+
+sandwich_make("whole wheat" ,["tomato", "cheese", "lettuce","meat"] ) 
+ #fourth 
+ #code wars
+
+ 
